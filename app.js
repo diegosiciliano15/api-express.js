0dcfc5819cdebc84a5dfc5ee9a2d27f07c6bcbf9
Alfonso Siciliano <alfonso.siciliano15@gmail.com>
2026-05-24 16:03:05 -0600

    Procesamiento de Formularios y JSON

--- app.js
+++ app.js
@@ -45,6 +45,31 @@ app.get('/search', (req, res) =>{
     }
 })
 
+app.post('/form', (req, res) =>{
+    const name = req.body.nombre || 'Anónimo';
+    const email = req.body.email || 'No proporcionado';
+
+    res.json({
+        message: 'Datos recibidos correctamente',
+        data: {
+            'nombre': name,
+            'email': email
+        }
+    })
+});
+
+app.post('/api/data', (req, res) =>{
+    const data = req.body;
+
+    if(!data || Object.keys(data).length === 0){
+        return res.status(400).json({error: 'No se recibieron datos en el cuerpo de la solicitud'});
+    }
+
+    res.status(200).json({
+        message: 'Datos recibidos correctamente',
+        data: data
+    })
+})
 
 app.listen(PORT, () =>{
     console.log('Servidor: http://localhost:' + PORT);
